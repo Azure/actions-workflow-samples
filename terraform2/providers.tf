5564afb00341b52dc48726e2dd4d73b1f7c78ec3
manojvazirani <mavazira@microsoft.com>
2020-08-06 15:53:23 -0400

    Update provider details

--- terraform2/providers.tf
+++ terraform2/providers.tf
@@ -1,3 +1,8 @@
 provider "azurerm" {
   version = "~> 1.44.0"
+
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.agent_client_id
+  client_secret   = var.agent_client_secret
 }
